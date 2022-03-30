a95847681a2bb5d2fd4925cc226948034b1f5984
QSP <QSP@QSP>
2022-03-30 07:54:19 +0530

    Autosuggtions

---
+++ src/autosuggestions/Test0.java
@@ -0,0 +1,42 @@
+package autosuggestions;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.google.com/");
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+WebElement ele = driver.findElement(By.name("q"));
+ele.sendKeys("seleni");
+
+Thread.sleep(3000);
+
+List<WebElement> suggestion = driver.findElements(By.xpath("//li[@class='sbct']"));
+System.out.println(suggestion.size());
+
+
+for(WebElement b:suggestion) {
+	System.out.println(b.getText());
+}
+
+driver.close();
+
+
+
+
+
+
+	}
+
+}
